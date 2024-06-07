597ee3542a8313d48ff20a9d8a29fa3294c08666
tangxianyun <xianyun.tang@ailabs.tw>
2024-06-07 23:27:42 +0800

    Added local sign in command

--- apps/backend/src/auth/commands/handlers/local-sign-in.handler.ts
+++ apps/backend/src/auth/commands/handlers/local-sign-in.handler.ts
@@ -1,10 +1,45 @@
+import { UnauthorizedException } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 
+import { UserRepository } from '../../../orm';
+import { JwtTokenService, PasswordService } from '../../services';
 import { LocalSignInCommand } from '../impl';
 
 @CommandHandler(LocalSignInCommand)
 export class LocalSignInHandler implements ICommandHandler<LocalSignInCommand> {
-  async execute() {
-    return undefined;
+  constructor(
+    private readonly passwordService: PasswordService,
+    private readonly jwtTokenService: JwtTokenService,
+    private readonly userRepository: UserRepository
+  ) {}
+
+  async execute(command: LocalSignInCommand) {
+    const { email, password } = command;
+
+    const exist = await this.userRepository.findOneOrFail(
+      {
+        email,
+      },
+      {
+        failHandler: () =>
+          new UnauthorizedException('Username and password do not match.'),
+      }
+    );
+
+    const hashedPassword = await this.passwordService.hashPassword(password);
+
+    const isPasswordMatched = await this.passwordService.isPasswordMatched(
+      password,
+      hashedPassword
+    );
+
+    if (!isPasswordMatched) {
+      throw new UnauthorizedException('Username and password do not match.');
+    }
+
+    return {
+      accessToken: this.jwtTokenService.issueAccessToken(exist),
+      refreshToken: this.jwtTokenService.issueRefreshToken(exist),
+    };
   }
 }
