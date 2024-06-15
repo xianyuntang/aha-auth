a228ce551bc8eedf481ffeb62776950efdf66186
tangxianyun <xianyun.tang@ailabs.tw>
2024-06-15 16:02:08 +0800

    Fixed scripts

--- scripts/build-and-push-image.sh
+++ scripts/build-and-push-image.sh
@@ -1,5 +1,5 @@
 docker build -t public.ecr.aws/f4r3h2b5/aha-backend -f apps/backend/Dockerfile .
-docker build -t public.ecr.aws/f4r3h2b5/aha-frontend -f apps/frotend/Dockerfile .
+docker build -t public.ecr.aws/f4r3h2b5/aha-frontend -f apps/frontend/Dockerfile .
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/f4r3h2b5
 docker push public.ecr.aws/f4r3h2b5/aha-backend
