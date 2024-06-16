ab60bc3d4fc06e4afc2ba59dac4c07e6c8c46a7d
tangxianyun <xianyun.tang@ailabs.tw>
2024-06-16 11:35:40 +0800

    Added firstname / lastname length limit

--- apps/backend/src/users/dto/update-my-profile.dto.ts
+++ apps/backend/src/users/dto/update-my-profile.dto.ts
@@ -1,10 +1,12 @@
-import { IsString } from 'class-validator';
+import { IsString, MaxLength } from 'class-validator';
 import { UpdateMyProfileRequest } from 'common';
 
 export class UpdateUserProfileRequestDto implements UpdateMyProfileRequest {
   @IsString()
+  @MaxLength(255)
   firstName!: string;
 
   @IsString()
+  @MaxLength(255)
   lastName!: string;
 }
